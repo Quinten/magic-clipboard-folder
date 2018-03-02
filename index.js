c6b33c5335d9ebf60f5c7d588df278f8f1dd8062
Quinten Clause <quinten.clause@studioemma.eu>
2018-03-02 22:08:17 +0100

    Add google vision detect text

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
 #!/usr/bin/env node --harmony
 
 console.log('Magic clipboard is watching your folder!');
+
+const vision = require('@google-cloud/vision');
+
+// Creates a client
+const client = new vision.ImageAnnotatorClient();
+
+/**
+  * TODO(developer): Uncomment the following line before running the sample.
+  */
+const fileName = 'test.png';
+
+// Performs text detection on the local file
+client.textDetection(fileName)
+    .then(results => {
+        const detections = results[0].textAnnotations;
+        console.log('Text:');
+        //detections.forEach(text => console.log(text));
+        if (detections[0]) {
+            console.log(detections[0].description);
+        }
+    })
+    .catch(err => {
+        console.error('ERROR:', err);
+    });
